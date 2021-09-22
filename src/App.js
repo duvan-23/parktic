790041f834a547b9bcaf74683c320b6bf30ba6e4
Duvan Caicedo <duvamendi2@hotmail.com>
2021-09-22 16:42:46 -0500

    first comit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const saludo = "Hola esta es mi primera linea en react...";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{saludo}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
