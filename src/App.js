d23917f8fe9559fef0b36838b3cc14017830f97c
Duvan Caicedo <duvamendi2@hotmail.com>
2021-09-22 16:52:30 -0500

    third commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 
 function App() {
-  const saludo = "Hola esta es mi primera linea en react...";
+  const saludo = "Hola esta es mi primera linea en react..";
   return (
     <div className="App">
       <header className="App-header">
